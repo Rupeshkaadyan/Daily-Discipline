a0737bb2a7714ed137ffb986c62f344ccb445adb
Rupeshkaadyan <134736518+Rupeshkaadyan@users.noreply.github.com>
2026-01-02 15:36:00 +0530

    Initial Commit

---
+++ Daily Discipline/Daily_DisciplineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_DisciplineApp.swift
+//  Daily Discipline
+//
+//  Created by Ethan on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Daily_DisciplineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
